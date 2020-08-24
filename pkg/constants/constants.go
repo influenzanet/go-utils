f31130422d8d06b5439b36d9b4e076ba38f6c0c2
phev8 <phev8@users.noreply.github.com>
2020-08-24 16:35:00 +0200

    Start collecting constants centrally

---
+++ pkg/constants/constants.go
@@ -0,0 +1,16 @@
+package constants
+
+const (
+	USER_ROLE_ADMIN       = "ADMIN"
+	USER_ROLE_RESEARCHER  = "RESEARCHER"
+	USER_ROLE_PARTICIPANT = "PARTICIPANT"
+)
+
+const (
+	EMAIL_TYPE_REGISTRATION       = "registration"
+	EMAIL_TYPE_VERIFY_EMAIL       = "verify-email"
+	EMAIL_TYPE_PASSWORD_RESET     = "password-reset"
+	EMAIL_TYPE_PASSWORD_CHANGED   = "password-changed"
+	EMAIL_TYPE_ACCOUNT_ID_CHANGED = "account-id-changed"
+	EMAIL_TYPE_STUDY_REMINDER     = "study-reminder"
+)
