a85ec8eb9fd08d5fe0498a8bcd3f049b619ff0a3
phev8 <phev8@users.noreply.github.com>
2020-08-24 22:59:44 +0200

    add email type for auth verification code

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -7,11 +7,12 @@ const (
 )
 
 const (
-	EMAIL_TYPE_REGISTRATION       = "registration"
-	EMAIL_TYPE_VERIFY_EMAIL       = "verify-email"
-	EMAIL_TYPE_PASSWORD_RESET     = "password-reset"
-	EMAIL_TYPE_PASSWORD_CHANGED   = "password-changed"
-	EMAIL_TYPE_ACCOUNT_ID_CHANGED = "account-id-changed"
-	EMAIL_TYPE_STUDY_REMINDER     = "study-reminder"
-	EMAIL_TYPE_ACCOUNT_DELETED    = "account-deleted"
+	EMAIL_TYPE_REGISTRATION           = "registration"
+	EMAIL_TYPE_VERIFY_EMAIL           = "verify-email"
+	EMAIL_TYPE_AUTH_VERIFICATION_CODE = "verification-code"
+	EMAIL_TYPE_PASSWORD_RESET         = "password-reset"
+	EMAIL_TYPE_PASSWORD_CHANGED       = "password-changed"
+	EMAIL_TYPE_ACCOUNT_ID_CHANGED     = "account-id-changed"
+	EMAIL_TYPE_STUDY_REMINDER         = "study-reminder"
+	EMAIL_TYPE_ACCOUNT_DELETED        = "account-deleted"
 )
