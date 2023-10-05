144a9b18e3670178a44958e2f2a033813182df3e
kschneider84 <77678452+kschneider84@users.noreply.github.com>
2023-10-05 09:31:02 +0200

    Add token purpose "inactive user notification"

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -23,10 +23,11 @@ const (
 )
 
 const (
-	TOKEN_PURPOSE_INVITATION             = "invitation"
-	TOKEN_PURPOSE_PASSWORD_RESET         = "password-reset"
-	TOKEN_PURPOSE_CONTACT_VERIFICATION   = "contact-verification"
-	TOKEN_PURPOSE_SURVEY_LOGIN           = "survey-login"
-	TOKEN_PURPOSE_UNSUBSCRIBE_NEWSLETTER = "unsubscribe-newsletter"
-	TOKEN_PURPOSE_RESTORE_ACCOUNT_ID     = "restore_account_id"
+	TOKEN_PURPOSE_INVITATION                 = "invitation"
+	TOKEN_PURPOSE_PASSWORD_RESET             = "password-reset"
+	TOKEN_PURPOSE_CONTACT_VERIFICATION       = "contact-verification"
+	TOKEN_PURPOSE_SURVEY_LOGIN               = "survey-login"
+	TOKEN_PURPOSE_UNSUBSCRIBE_NEWSLETTER     = "unsubscribe-newsletter"
+	TOKEN_PURPOSE_RESTORE_ACCOUNT_ID         = "restore_account_id"
+	TOKEN_PURPOSE_INACTIVE_USER_NOTIFICATION = "inactive-user-notification"
 )
