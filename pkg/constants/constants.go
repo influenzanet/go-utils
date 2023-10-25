60e6decac82e5c618a65d3a4756035d678a4b8a9
kschneider84 <77678452+kschneider84@users.noreply.github.com>
2023-10-25 11:42:53 +0200

    Add email type "account deleted after inactivity"

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -8,18 +8,19 @@ const (
 )
 
 const (
-	EMAIL_TYPE_REGISTRATION           = "registration"
-	EMAIL_TYPE_INVITATION             = "invitation"
-	EMAIL_TYPE_VERIFY_EMAIL           = "verify-email"
-	EMAIL_TYPE_AUTH_VERIFICATION_CODE = "verification-code"
-	EMAIL_TYPE_PASSWORD_RESET         = "password-reset"
-	EMAIL_TYPE_PASSWORD_CHANGED       = "password-changed"
-	EMAIL_TYPE_ACCOUNT_ID_CHANGED     = "account-id-changed"
-	EMAIL_TYPE_WEEKLY                 = "weekly"
-	EMAIL_TYPE_STUDY_REMINDER         = "study-reminder"
-	EMAIL_TYPE_NEWSLETTER             = "newsletter"
-	EMAIL_TYPE_ACCOUNT_DELETED        = "account-deleted"
-	EMAIL_TYPE_ACCOUNT_INACTIVITY     = "account-inactivity"
+	EMAIL_TYPE_REGISTRATION                     = "registration"
+	EMAIL_TYPE_INVITATION                       = "invitation"
+	EMAIL_TYPE_VERIFY_EMAIL                     = "verify-email"
+	EMAIL_TYPE_AUTH_VERIFICATION_CODE           = "verification-code"
+	EMAIL_TYPE_PASSWORD_RESET                   = "password-reset"
+	EMAIL_TYPE_PASSWORD_CHANGED                 = "password-changed"
+	EMAIL_TYPE_ACCOUNT_ID_CHANGED               = "account-id-changed"
+	EMAIL_TYPE_WEEKLY                           = "weekly"
+	EMAIL_TYPE_STUDY_REMINDER                   = "study-reminder"
+	EMAIL_TYPE_NEWSLETTER                       = "newsletter"
+	EMAIL_TYPE_ACCOUNT_DELETED                  = "account-deleted"
+	EMAIL_TYPE_ACCOUNT_DELETED_AFTER_INACTIVITY = "account-deleted-after-inactivity"
+	EMAIL_TYPE_ACCOUNT_INACTIVITY               = "account-inactivity"
 )
 
 const (
